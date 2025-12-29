0e685959acff99551b570797622f8062bef559ee
Axel Denis <axel.denis@outlook.fr>
2025-12-29 15:21:03 +0100

    feat: numpad macros script

---
+++ hypr/scripts/numpad_macros.sh
@@ -0,0 +1,8 @@
+EVENT=$(sudo cat /proc/bus/input/devices | grep -A 4 "\"Hengchangtong  HCT USB Entry Keyboard\"" | grep -o "event[[:digit:]]")
+
+sudo evsieve --input "/dev/input/${EVENT}" grab \
+--map "key:kp1" "key:leftshift" "key:leftmeta" "key:f1" \
+--map "key:kp2" "key:leftshift" "key:leftmeta" "key:f2" \
+--map "key:kp3" "key:leftshift" "key:leftmeta" "key:f3" \
+--map "key:kpenter" "key:leftctrl" "key:leftmeta" "key:f" \
+--output &
